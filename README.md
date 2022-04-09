3c1b41da7c6b54856257728e09d8b2a22f547bae
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2022-04-09 15:11:09 +0700

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# SpringBoot-Cloud-Kafka-SSL
---
+++ src/main/java/com/tirmizee/SpringBootCloudKafkaSslApplication.java
@@ -0,0 +1,13 @@
+package com.tirmizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudKafkaSslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCloudKafkaSslApplication.class, args);
+	}
+
+}
